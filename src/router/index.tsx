6f2fbfc10c9cbb9d0a5230c8de2da2d80c929858
Ivan Belikov <belikov.workmail@gmail.com>
2024-10-24 00:05:56 +0300

    hardcoded routes moved to constants

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import ROUTES from './routes'
+
 import App from '../App'
 
 const router = createBrowserRouter([
 	{
-		path: '/',
+		path: ROUTES.home,
 		element: <App />,
 	},
 	{
-		path: '/app/:page?',
+		path: ROUTES.app(':page?'),
 		element: <div>App</div>,
 	},
 	{
-		path: '/admin/:page?',
+		path: ROUTES.admin(':page?'),
 		element: <div>Admin</div>,
 	},
 ])
