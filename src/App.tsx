60a75e229ea2f0acc1f4446af296a8b26f70b5a1
alena_gluschenko <qlushenko36@gmail.com>
2024-10-26 18:34:03 +0300

    updated import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { ThemeProvider } from './theme/theme-provider/theme-provider'
-import { CssBaseline } from '@mui/material'
+import CssBaseline from '@mui/material/CssBaseline'
 
 function App() {
   const [count, setCount] = useState(0)
