57bcfb6efded5778bed5c06e8b7758cf8bd9f224
Slumrag <mrgreenthecristal@yandex.ru>
2024-10-27 21:52:13 +0300

    chore: :wrench: configure aliases \w tsconfigPaths

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})
+  plugins: [react(), tsconfigPaths()],
+});
