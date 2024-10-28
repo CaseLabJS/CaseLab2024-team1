5ab1d3b546865ed1bd7f6a5c7cecb482620a07a0
gineff <canone@inbox.ru>
2024-10-28 21:55:45 +0300

    feat: :sparkles: add attribute api

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,50 @@
 import './App.css'
-import { documentTypesControllerApi } from './api/documentTypesController'
+import { attributeControllerApi } from './api/attributeController'
 
 function App() {
   const model = {
-    name: 'new document extra type',
-    attributes: [
-      {
-        id: 1,
-        name: 'testAttr1',
-        required: false,
-      },
-    ],
+    documentTypesNames: ['testType'],
+    name: 'extraAttr1',
+    required: false,
   }
 
-  const api = documentTypesControllerApi
+  const api = attributeControllerApi
 
   const create = () => {
-    api.createDocumentTypes(model).then((data) => console.log(data))
+    api
+      .createAttribute({
+        ...model,
+      })
+      .then((data) => console.log(data))
   }
 
   const get = () => {
-    api.getDocumentTypesById(1).then((data) => console.log(data))
+    api.getAttributeById(3).then((data) => console.log(data))
   }
 
   const patch = () => {
     api
-      .patchDocumentTypes(3, { name: 'new document extra mega type' })
+      .patchAttribute(3, { name: 'new document extra mega type' })
       .then((data) => console.log(data))
   }
 
   const update = () => {
     api
-      .updateDocumentTypes({
+      .updateAttribute({
         ...model,
         id: 3,
-        attributes: [
-          ...model.attributes,
-          {
-            id: 2,
-            name: 'testAttr2',
-            required: true,
-          },
-        ],
+        required: true,
+        documentTypesNames: ['new document extra type', 'testType'],
       })
       .then((data) => console.log(data))
   }
 
   const remove = () => {
-    api.deleteDocumentTypes(2).then((data) => console.log(data))
+    api.deleteAttribute(2).then((data) => console.log(data))
   }
 
   const getAll = () => {
-    api.getDocumentsTypes().then((data) => console.log(data))
+    api.getAttributes().then((data) => console.log(data))
   }
 
   return (
