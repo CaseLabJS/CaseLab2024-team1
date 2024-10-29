98ec160631491134227b2f6b54f7d669e667e2c8
Ivan Belikov <belikov.workmail@gmail.com>
2024-10-29 11:24:04 +0300

    refactor: :recycle: Router Refactor

--- src/router/routes.ts
+++
@@ -1,7 +0,0 @@
-const ROUTES = {
-  home: '/',
-  admin: (page: string) => `/admin/${page}`,
-  app: (page: string) => `/app/${page}`,
-}
-
-export default ROUTES
---
+++ src/router/routes.tsx
@@ -0,0 +1,48 @@
+import App from '@/App'
+
+const ROUTES = {
+  home: '/',
+  admin: (page = '') => `/admin/${page}`,
+  app: (page = '') => `/app/${page}`,
+}
+
+export const publicRoutes = [
+  {
+    path: '*',
+    element: <div>404 Not Found</div>,
+  },
+  {
+    path: ROUTES.home,
+    element: <App />,
+  },
+]
+
+export const adminRoutes = [
+  {
+    path: ROUTES.admin(),
+    element: <div>Admin</div>,
+  },
+  {
+    path: ROUTES.admin('users'),
+    element: <div>Users</div>,
+  },
+  {
+    path: ROUTES.admin('document-type'),
+    element: <div>Document Type</div>,
+  },
+  {
+    path: ROUTES.admin('attribute-type'),
+    element: <div>Attribute Type</div>,
+  },
+]
+
+export const appRoutes = [
+  {
+    path: ROUTES.app(),
+    element: <div>App</div>,
+  },
+  {
+    path: ROUTES.app('document'),
+    element: <div>Create Document</div>,
+  },
+]
