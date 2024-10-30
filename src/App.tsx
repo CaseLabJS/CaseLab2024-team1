2e151e04f64d966af177765a7a893ff11e4cc7b5
Ivan Belikov <belikov.workmail@gmail.com>
2024-10-30 23:53:29 +0300

    RouterProvider moved to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from './theme/theme-provider/theme-provider'
-import CssBaseline from '@mui/material/CssBaseline'
+
+import router from '@/router'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <ThemeProvider>
       <RouterProvider router={router} />
