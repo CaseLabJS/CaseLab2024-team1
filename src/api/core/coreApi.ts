fe3127a6a747a454ab77e8b72edc29f9bf38e968
gineff <canone@inbox.ru>
2024-10-31 21:02:23 +0300

    feat: add token to private api interceptors & add SerializedError

--- src/api/core/coreApi.ts
+++ src/api/core/coreApi.ts
@@ -1,54 +1,51 @@
 import axios, {
-    type AxiosError,
-    AxiosRequestConfig,
-    type AxiosResponse,
+  type AxiosError,
+  AxiosRequestConfig,
+  type AxiosResponse,
 } from 'axios'
 import { apiPath } from '@/config'
 import { RequestType, RequestTypeWithData } from '../types'
+import { token } from '@/lib/tokenManager'
+import { SerializedError } from './serializedError'
 
 export abstract class CoreApi {
-    protected api = axios.create({
-        baseURL: apiPath,
-    })
+  protected api = axios.create({
+    baseURL: apiPath,
+  })
 
-    constructor() {
-        this.api.interceptors.response.use(
-            (response: AxiosResponse) => response.data,
-            (error: AxiosError) => {
-                const { response, code } = error
-                const statusCode = response?.status
-                console.error(error, statusCode)
+  constructor() {
+    this.api.interceptors.response.use(
+      <T>(response: AxiosResponse<T>) => response.data,
+      (error: AxiosError<string>) => {
+        const { status, data } = error.response ?? {
+          status: error.status,
+          data: 'Unknown error',
+        }
 
-                if (statusCode === 401) {
-                    return Promise.reject(error)
-                }
+        if (error.status === 401) {
+          token.clear()
+        }
 
-                if (statusCode === 404) {
-                    return Promise.reject(error)
-                }
-
-                if (statusCode === 500) {
-                    return Promise.reject(error)
-                }
-
-                return Promise.reject({
-                    ...(statusCode && { code: statusCode ?? code }),
-                    message: JSON.stringify(error),
-                })
-            }
+        return Promise.reject(
+          new SerializedError({
+            status,
+            message: data,
+          })
         )
-    }
+      }
+    )
+  }
 
-    get: RequestType = (...req) => this.api.get(...req)
+  get: RequestType = (...req) => this.api.get(...req)
 
-    post: RequestTypeWithData = (...req) => this.api.post(...req)
+  post: RequestTypeWithData = (...req) => this.api.post(...req)
 
-    patch: RequestTypeWithData = (...req) => this.api.patch(...req)
+  patch: RequestTypeWithData = (...req) => this.api.patch(...req)
 
-    put: RequestTypeWithData = (...req) => this.api.put(...req)
+  put: RequestTypeWithData = (...req) => this.api.put(...req)
 
-    delete: RequestType = (...req) => this.api.delete(...req)
+  delete: RequestType = (...req) => this.api.delete(...req)
 
-    request = <D>(config: AxiosRequestConfig<D>): Promise<D> =>
-        this.api.request(config)
+  request = <D>(config: AxiosRequestConfig<D>): Promise<D> =>
+    this.api.request(config)
 }
