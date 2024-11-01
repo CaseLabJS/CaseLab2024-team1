9e77273c55e7efc8ff0c491120bab5272ef81152
gineff <canone@inbox.ru>
2024-11-01 13:56:05 +0300

    fix: remove auth store realization

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,43 +1,18 @@
 import { makeAutoObservable } from 'mobx'
-import { authControllerApi } from '@/api/authController'
-import { token } from '@/lib/tokenManager'
+//import { authControllerApi } from '@/api/authController'
+//import { token } from '@/lib/tokenManager'
 import { SerializedError } from '@/api/core/serializedError'
 
 class AuthStore {
   loading = false
   error: SerializedError | null = null
-
+  isAuth = false
+  isAdmin = false
   constructor() {
     makeAutoObservable(this)
   }
-  async login(
-    { email, password }: { email: string; password: string },
-    rememberMe: boolean = false
-  ) {
-    this.loading = true
-    this.error = null
-
-    try {
-      const { token: tokenValue } = await authControllerApi.login({
-        email,
-        password,
-      })
-      token.storageType = rememberMe ? localStorage : sessionStorage
-      token.value = tokenValue
-    } catch (error) {
-      this.error = error as SerializedError
-    } finally {
-      this.loading = false
-    }
-  }
-
-  get isAuth() {
-    return token.value && !token.isExpired
-  }
-
-  get isAdmin() {
-    return token.payload?.sub === 'admin'
-  }
+  login = async () => {}
+  logout = async () => {}
 }
 
 export const authStore = new AuthStore()
