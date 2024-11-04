7c447312b330dbe1319eb4f6dcdc1b8e9a8718d7
Ivan Belikov <belikov.workmail@gmail.com>
2024-11-04 13:08:20 +0300

    DocumentTypeListStore added

---
+++ src/stores/DocumentTypeListStore/DocumentTypeListStore.ts
@@ -0,0 +1,62 @@
+import { documentTypesControllerApi } from '@/api/documentTypesController'
+import DocumentTypeStore from '@/stores/DocumentTypeStore'
+import { NewDocumentType } from '@/types/sharedTypes'
+import { executeWithLoading } from '@/utils/executeWithLoading'
+import { makeAutoObservable, runInAction } from 'mobx'
+
+class DocumentTypeListStore {
+  types: InstanceType<typeof DocumentTypeStore>[] = []
+  loading: boolean = false
+  error: string | null = null
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  async fetchDocumentTypes() {
+    const fetchedTypes = await executeWithLoading(this, async () =>
+      documentTypesControllerApi.getDocumentTypes()
+    )
+
+    if (fetchedTypes) {
+      runInAction(() => {
+        this.types = [
+          ...fetchedTypes.map((type) => new DocumentTypeStore(type)),
+        ]
+      })
+    }
+  }
+
+  async getDocumentTypeById(typeId: number) {
+    const type = await executeWithLoading(this, () =>
+      documentTypesControllerApi.getDocumentTypeById(typeId)
+    )
+
+    return type || null
+  }
+
+  async createDocumentType(type: NewDocumentType) {
+    const newType = await executeWithLoading(this, () =>
+      documentTypesControllerApi.createDocumentType(type)
+    )
+
+    if (newType) {
+      runInAction(() => {
+        this.types = [...this.types, new DocumentTypeStore(newType)]
+      })
+    }
+  }
+
+  async deleteDocumentType(typeId: number) {
+    await executeWithLoading(this, () =>
+      documentTypesControllerApi.deleteDocumentTypes(typeId)
+    )
+
+    runInAction(() => {
+      this.types = [...this.types.filter((type) => type.data.id !== typeId)]
+    })
+  }
+}
+
+const documentTypeListStore = new DocumentTypeListStore()
+export default documentTypeListStore
