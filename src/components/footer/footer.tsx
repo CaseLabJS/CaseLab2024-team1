94748d8a54865e7395fd04cc8209d7a31e3a9d9f
Elena Lisichenok <fjernt.land@gmail.com>
2024-11-04 10:13:30 +0300

    refactor: :art: remove the authStore props and use it directly inside the component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,58 +3,56 @@ import { observer } from 'mobx-react-lite'
 import { Box, Typography, Link } from '@mui/material'
 import authStore from '@/stores/authStore'
 
-const Footer: React.FC<{ authStore: typeof authStore }> = observer(
-  ({ authStore }) => {
-    const { isAuth, isAdmin } = authStore
+const Footer: React.FC = observer(() => {
+  const { isAuth, isAdmin } = authStore
 
-    const linkStyles = {
-      textDecoration: 'none',
-      color: 'primary.main',
-      '&:hover': {
-        color: 'primary.dark',
-      },
-      '&:focus': {
-        color: 'primary.light',
-      },
-    }
-
-    const renderLink = (href: string, text: string) => (
-      <Link href={href} variant="body2" sx={linkStyles}>
-        {text}
-      </Link>
-    )
-
-    const commonContent = (
-      <>
-        <Typography variant="h6">Лого</Typography>
-        {isAdmin
-          ? renderLink('#', 'Документация')
-          : isAuth
-            ? renderLink('#', 'Часто задаваемые вопросы')
-            : null}
-        <Typography variant="body2">© 2024</Typography>
-      </>
-    )
-
-    return (
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: { xs: 'column', sm: 'row' },
-          justifyContent: 'space-between',
-          alignItems: 'baseline',
-          p: { xs: '1rem', sm: '1.5rem' },
-          backgroundColor: 'background.default',
-          textAlign: 'center',
-          minWidth: '20rem',
-          maxWidth: '80rem',
-          margin: '0 auto',
-        }}
-      >
-        {commonContent}
-      </Box>
-    )
+  const linkStyles = {
+    textDecoration: 'none',
+    color: 'primary.main',
+    '&:hover': {
+      color: 'primary.dark',
+    },
+    '&:focus': {
+      color: 'primary.light',
+    },
   }
-)
+
+  const renderLink = (href: string, text: string) => (
+    <Link href={href} variant="body2" sx={linkStyles}>
+      {text}
+    </Link>
+  )
+
+  const commonContent = (
+    <>
+      <Typography variant="h6">Лого</Typography>
+      {isAdmin
+        ? renderLink('#', 'Документация')
+        : isAuth
+          ? renderLink('#', 'Часто задаваемые вопросы')
+          : null}
+      <Typography variant="body2">© 2024</Typography>
+    </>
+  )
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: { xs: 'column', sm: 'row' },
+        justifyContent: 'space-between',
+        alignItems: 'baseline',
+        p: { xs: '1rem', sm: '1.5rem' },
+        backgroundColor: 'background.default',
+        textAlign: 'center',
+        minWidth: '20rem',
+        maxWidth: '80rem',
+        margin: '0 auto',
+      }}
+    >
+      {commonContent}
+    </Box>
+  )
+})
 
 export default Footer
