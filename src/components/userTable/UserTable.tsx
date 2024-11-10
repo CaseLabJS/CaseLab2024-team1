f4b7f9cdf26b087f6c59710792ddcf2a0ef404b2
Tatiana <tvtugova1511@gmail.com>
2024-11-10 23:10:36 +0300

    feat: user deletion functionality added

--- src/components/userTable/UserTable.tsx
+++ src/components/userTable/UserTable.tsx
@@ -1,25 +1,44 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { DataGrid, GridColDef } from '@mui/x-data-grid'
-import { Button, Typography, Box, Paper } from '@mui/material'
 import { User, Role } from '@/types/sharedTypes'
-import { observer } from 'mobx-react-lite'
 import { usersListStore } from '@/stores/UsersListStore'
+import { observer } from 'mobx-react-lite'
+import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid'
+import {
+  Button,
+  Typography,
+  Box,
+  Paper,
+  Popover,
+  IconButton,
+} from '@mui/material'
+import { Delete as DeleteIcon, Close as CloseIcon } from '@mui/icons-material'
 
 const UserTable: React.FC = observer(() => {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null)
+  const [popoverIsOpen, setPopoverIsOpen] = useState(false)
+  const [idToDelete, setIdToDelete] = useState<number>(0)
   const navigate = useNavigate()
   const handleAddUser = () => {
     navigate('/admin/users/create')
   }
+  const handleClose = () => {
+    setAnchorEl(null)
+    setPopoverIsOpen(false)
+  }
+  const handleDelete = (id: number) => {
+    void usersListStore.deleteUser(id)
+    handleClose()
+  }
   useEffect(() => {
     void usersListStore.fetchUsers()
   }, [])
-  const rows: User[] = usersListStore.users.map((user) => ({
-    id: user.userData.id,
-    name: user.userData.name,
-    surname: user.userData.surname,
-    email: user.userData.email,
-    roles: user.userData.roles,
+  const rows: User[] = usersListStore.users.map(({ userData }) => ({
+    id: userData.id,
+    name: userData.name,
+    surname: userData.surname,
+    email: userData.email,
+    roles: userData.roles,
   }))
   const columns: GridColDef[] = [
     {
@@ -50,6 +69,23 @@ const UserTable: React.FC = observer(() => {
         return `${roles[0].name}`
       },
     },
+    {
+      field: 'actions',
+      headerName: 'Действия',
+      width: 200,
+      renderCell: (params: GridRenderCellParams<User>) => (
+        <IconButton
+          aria-label="delete"
+          onClick={(event) => {
+            setAnchorEl(event.currentTarget)
+            setPopoverIsOpen(true)
+            setIdToDelete(params.row.id)
+          }}
+        >
+          <DeleteIcon />
+        </IconButton>
+      ),
+    },
   ]
 
   return (
@@ -72,6 +108,34 @@ const UserTable: React.FC = observer(() => {
           disableRowSelectionOnClick
         />
       </Box>
+      <Popover
+        open={popoverIsOpen}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <Box
+          sx={{
+            padding: 1,
+            display: 'flex',
+            alignItems: 'center',
+            borderRadius: '26px',
+          }}
+        >
+          <Typography>Вы уверены?</Typography>
+          <Button onClick={() => handleDelete(idToDelete)}>Да</Button>
+          <IconButton aria-label="close" onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
+      </Popover>
     </Paper>
   )
 })
