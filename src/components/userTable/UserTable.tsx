63b4d711f269c49308a09b67b9024ee48ec53590
Tatiana <tvtugova1511@gmail.com>
2024-11-12 15:28:32 +0300

    dinamic route, error and loading handling added

--- src/components/userTable/UserTable.tsx
+++ src/components/userTable/UserTable.tsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { ROUTES } from '@/router/constants.ts'
+import { Loader } from '@/components/loader/loader'
 import { User, Role } from '@/types/sharedTypes'
 import { usersListStore } from '@/stores/UsersListStore'
 import { observer } from 'mobx-react-lite'
 import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid'
-import { Box, IconButton, Paper, Typography, Button } from '@mui/material'
+import {
+  Box,
+  IconButton,
+  Paper,
+  Typography,
+  Button,
+  Modal,
+  Alert,
+  Snackbar,
+} from '@mui/material'
 import { Delete as DeleteIcon, Edit as EditIcon } from '@mui/icons-material'
 import ConfirmPopover from './ConfirmPopover'
 
@@ -12,21 +23,30 @@ const UserTable: React.FC = observer(() => {
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null)
   const [popoverIsOpen, setPopoverIsOpen] = useState(false)
   const [idToDelete, setIdToDelete] = useState<number>(0)
+  const [loaderIsOpen, setLoaderIsOpen] = useState(true)
+  const [snackbarIsOpen, setSnackbarIsOpen] = useState(false)
+  const { loading, error } = usersListStore
   const navigate = useNavigate()
   const handleAddUser = () => {
-    navigate('/admin/users/create')
+    navigate(ROUTES.admin('users/create'))
   }
   const handleClose = () => {
     setAnchorEl(null)
     setPopoverIsOpen(false)
   }
-  const handleDelete = (id: number) => {
-    void usersListStore.deleteUser(id)
+  const handleDelete = async (id: number) => {
+    await usersListStore.deleteUser(id)
+    setSnackbarIsOpen(true)
     handleClose()
   }
   useEffect(() => {
     void usersListStore.fetchUsers()
   }, [])
+  useEffect(() => {
+    if (error) {
+      setSnackbarIsOpen(true)
+    }
+  }, [error])
   const rows: User[] = usersListStore.users.map(({ userData }) => ({
     id: userData.id,
     name: userData.name,
@@ -118,9 +138,35 @@ const UserTable: React.FC = observer(() => {
         open={popoverIsOpen}
         anchorEl={anchorEl}
         title="Вы уверены?"
-        onConfirm={() => handleDelete(idToDelete)}
+        onConfirm={() => void handleDelete(idToDelete)}
         onClose={handleClose}
       />
+      <Snackbar
+        open={snackbarIsOpen}
+        autoHideDuration={6000}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        onClose={() => setSnackbarIsOpen(false)}
+      >
+        <Alert severity={error ? 'error' : 'success'} sx={{ width: '100%' }}>
+          {error ? error.message : 'Пользователь удален'}
+        </Alert>
+      </Snackbar>
+      <Modal
+        open={loading && loaderIsOpen}
+        onClick={() => setLoaderIsOpen(false)}
+        closeAfterTransition
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}
+        >
+          <Loader />
+        </Box>
+      </Modal>
     </Paper>
   )
 })
