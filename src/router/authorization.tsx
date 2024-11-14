c6483006507ea7b5cd8d3c3e31d4bff3e60dab62
gineff <canone@inbox.ru>
2024-11-14 13:16:44 +0300

    fix: :bug: pretect admin rout

--- src/router/authorization.tsx
+++ src/router/authorization.tsx
@@ -1,5 +1,5 @@
 import type { FC } from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
 import authStore from '@/stores/AuthStore'
 import type { AuthorizationProps } from './types'
@@ -7,6 +7,7 @@ import { ROUTES } from '@/router/constants.ts'
 
 export const Authorization: FC<AuthorizationProps> = observer(
   ({ requireAuth }) => {
+    const location = useLocation()
     const { isAuth, isAdmin } = authStore
     // маршрут /sign-in если пользователь уже авторизован, редирект на домашнюю страницу согласно роли
     if (!requireAuth && isAuth) {
@@ -15,6 +16,12 @@ export const Authorization: FC<AuthorizationProps> = observer(
     // route, требующий авторизацию, и пользователь не авторизован -> редирект на авторизацию
     if (requireAuth && !isAuth) {
       return <Navigate to={ROUTES.signIn} />
+    }
+
+    // защита маршрута /admin* от обычного пользователя
+    const rootAdminPath = ROUTES.admin().replace(/\/$/, '')
+    if (location.pathname.startsWith(rootAdminPath) && !isAdmin) {
+      return <Navigate to={ROUTES.app()} replace />
     }
 
     return <Outlet />
