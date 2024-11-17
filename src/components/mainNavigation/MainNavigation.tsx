9cec795f03de19baf115f9fbbc8a865bab4f902a
Elena Lisichenok <fjernt.land@gmail.com>
2024-11-17 20:52:54 +0300

    feat: :sparkles: add drawer component

--- src/components/mainNavigation/MainNavigation.tsx
+++ src/components/mainNavigation/MainNavigation.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
 import authStore from '@/stores/AuthStore'
 import { ROUTES } from '@/router/constants'
-import { List, ListItem, ListItemButton, ListItemText } from '@mui/material'
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  IconButton,
+  Drawer,
+} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+
+interface MainNavLinksProps {
+  isAdmin: boolean
+  isAuth: boolean
+  adminLinks: Array<{ text: string; path: string }>
+  userLinks: Array<{ text: string; path: string }>
+}
 
 const MainNavigation: React.FC = observer(() => {
   const { isAuth, isAdmin } = authStore
+  const [drawerOpen, setDrawerOpen] = useState(false)
 
   const userLinks = [
     { text: 'Панель управления', path: ROUTES.app() },
@@ -19,12 +35,76 @@ const MainNavigation: React.FC = observer(() => {
     { text: 'Создать пользователя', path: ROUTES.admin('users/create') },
   ]
 
+  const MainNavLinks: React.FC<MainNavLinksProps> = ({
+    isAdmin,
+    isAuth,
+    adminLinks,
+    userLinks,
+  }) => {
+    const links = isAdmin ? adminLinks : isAuth ? userLinks : []
+
+    return (
+      <>
+        {links.map((link) => (
+          <ListItem
+            key={link.text}
+            disablePadding
+            sx={{ width: 'fit-content', textTransform: 'uppercase' }}
+          >
+            <ListItemButton component={Link} to={link.path}>
+              <ListItemText primary={link.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </>
+    )
+  }
+
+  const toggleDrawer = (open: boolean) => () => {
+    setDrawerOpen(open)
+  }
+
   return (
     <nav>
+      <IconButton
+        onClick={drawerOpen ? toggleDrawer(false) : toggleDrawer(true)}
+        sx={{
+          display: { xs: 'block', md: 'none' },
+          position: 'absolute',
+          top: '1.5rem',
+          right: '1.5rem',
+          width: '2rem',
+          height: '2rem',
+          zIndex: 10000,
+        }}
+      >
+        <MenuIcon
+          sx={{
+            position: 'inherit',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            margin: 'auto',
+          }}
+        />
+      </IconButton>
+
+      <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
+        <List sx={{ width: '20rem', marginTop: '7vh', padding: 2 }}>
+          <MainNavLinks
+            isAdmin={isAdmin}
+            isAuth={isAuth}
+            adminLinks={adminLinks}
+            userLinks={userLinks}
+          />
+        </List>
+      </Drawer>
+
       <List
         sx={{
-          display: 'flex',
-          flexDirection: { xs: 'column', md: 'row' },
+          display: { xs: 'none', md: 'flex' },
+          flexDirection: { md: 'row' },
           flexWrap: 'wrap',
           justifyContent: 'center',
           gap: 1.5,
@@ -32,30 +112,12 @@ const MainNavigation: React.FC = observer(() => {
           margin: 0,
         }}
       >
-        {isAdmin &&
-          adminLinks.map((link) => (
-            <ListItem
-              key={link.text}
-              disablePadding
-              sx={{ width: 'fit-content', margin: { xs: '0 auto', md: '0' } }}
-            >
-              <ListItemButton component={Link} to={link.path}>
-                <ListItemText
-                  primary={link.text}
-                  sx={{ textAlign: 'center' }}
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        {isAuth &&
-          !isAdmin &&
-          userLinks.map((link) => (
-            <ListItem key={link.text} disablePadding>
-              <ListItemButton component={Link} to={link.path}>
-                <ListItemText primary={link.text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+        <MainNavLinks
+          isAdmin={isAdmin}
+          isAuth={isAuth}
+          adminLinks={adminLinks}
+          userLinks={userLinks}
+        />
       </List>
     </nav>
   )
