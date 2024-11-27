62360a45676b49cfcac20b5af335d95190bfa623
alena_gluschenko <qlushenko36@gmail.com>
2024-11-27 16:48:38 +0300

    the extra observer has been removed

--- src/components/documentsList/documentsList.tsx
+++ src/components/documentsList/documentsList.tsx
@@ -15,7 +15,6 @@ import { useTheme } from '@mui/material'
 import { alpha } from '@mui/material/styles'
 import { GridPaginationModel } from '@mui/x-data-grid/models/gridPaginationProps'
 import { ToolbarButton } from '@/components/documentsList/types.ts'
-import { observer } from 'mobx-react-lite'
 
 export const DEFAULT_PAGE_SIZE = 15
 export const DEFAULT_PAGE = 0
@@ -34,119 +33,117 @@ interface DocumentsListProps<T extends GridValidRowModel> {
   onPaginationModelChange?: (paginationModel: GridPaginationModel) => void
 }
 
-export const DocumentsList = observer(
-  <T extends GridValidRowModel>(props: DocumentsListProps<T>) => {
-    const {
-      rows,
-      columns,
-      totalDocuments,
-      initialPage = DEFAULT_PAGE,
-      initialPageSize = DEFAULT_PAGE_SIZE,
-      paginationOption = DEFAULT_PAGINATION_OPTIONS,
-      buttons,
-      selectionModel,
-      onSelectionChange,
-      onPaginationModelChange,
-    } = props
+export const DocumentsList = <T extends GridValidRowModel>(
+  props: DocumentsListProps<T>
+) => {
+  const {
+    rows,
+    columns,
+    totalDocuments,
+    initialPage = DEFAULT_PAGE,
+    initialPageSize = DEFAULT_PAGE_SIZE,
+    paginationOption = DEFAULT_PAGINATION_OPTIONS,
+    buttons,
+    selectionModel,
+    onSelectionChange,
+    onPaginationModelChange,
+  } = props
 
-    const [paginationModel, setPaginationModel] = useState<GridPaginationModel>(
-      {
-        page: initialPage,
-        pageSize: initialPageSize,
+  const [paginationModel, setPaginationModel] = useState<GridPaginationModel>({
+    page: initialPage,
+    pageSize: initialPageSize,
+  })
+
+  const navigate = useNavigate()
+  const theme = useTheme()
+
+  const handleRowClick = useCallback(
+    (params: GridRowParams) => {
+      navigate(`${params.id}`)
+    },
+    [navigate]
+  )
+
+  const handleRowSelectionModelChange = useCallback(
+    (newSelectionModel: GridRowSelectionModel) => {
+      if (onSelectionChange) {
+        onSelectionChange(newSelectionModel)
       }
-    )
+    },
+    [onSelectionChange]
+  )
 
-    const navigate = useNavigate()
-    const theme = useTheme()
+  const handlePaginationModelChange = useCallback(
+    (paginationModel: GridPaginationModel) => {
+      setPaginationModel(paginationModel)
+      if (onPaginationModelChange) {
+        onPaginationModelChange(paginationModel)
+      }
+    },
+    [onPaginationModelChange]
+  )
 
-    const handleRowClick = useCallback(
-      (params: GridRowParams) => {
-        navigate(`${params.id}`)
-      },
-      [navigate]
-    )
-
-    const handleRowSelectionModelChange = useCallback(
-      (newSelectionModel: GridRowSelectionModel) => {
-        if (onSelectionChange) {
-          onSelectionChange(newSelectionModel)
-        }
-      },
-      [onSelectionChange]
-    )
-
-    const handlePaginationModelChange = useCallback(
-      (paginationModel: GridPaginationModel) => {
-        setPaginationModel(paginationModel)
-        if (onPaginationModelChange) {
-          onPaginationModelChange(paginationModel)
-        }
-      },
-      [onPaginationModelChange]
-    )
-
-    return (
-      <>
-        <Paper>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            paginationMode="server"
-            paginationModel={paginationModel}
-            onPaginationModelChange={handlePaginationModelChange}
-            rowCount={totalDocuments}
-            pageSizeOptions={paginationOption}
-            checkboxSelection
-            disableColumnMenu
-            disableRowSelectionOnClick
-            onRowSelectionModelChange={handleRowSelectionModelChange}
-            rowSelectionModel={selectionModel}
-            onRowClick={handleRowClick}
-            localeText={{
-              footerRowSelected: (count) => `${count} выбрано`,
-              noResultsOverlayLabel: 'Нет результатов',
-              toolbarColumns: 'Столбцы',
-              toolbarColumnsLabel: 'Управление столбцами',
-              toolbarFilters: 'Фильтры',
-              toolbarFiltersLabel: 'Фильтры',
-              toolbarExport: 'Экспорт',
-              toolbarExportLabel: 'Экспортировать',
-              toolbarExportCSV: 'Экспорт в CSV',
-              toolbarExportPrint: 'Печать',
-              toolbarExportExcel: 'Экспорт в Excel',
-            }}
-            slots={{
-              pagination: (props) => (
-                <GridPagination
-                  {...props}
-                  labelDisplayedRows={({ from, to, count }) => {
-                    return ` ${from}-${to} из ${count}`
-                  }}
-                  labelRowsPerPage="Документов на странице:"
-                />
-              ),
-              toolbar: () => <GridToolbar buttons={buttons} />,
-            }}
-            sx={{
-              '--DataGrid-overlayHeight': '300px',
-              '& .MuiDataGrid-cell': {
-                '&:focus': {
-                  outline: 'none',
-                },
+  return (
+    <>
+      <Paper>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          paginationMode="server"
+          paginationModel={paginationModel}
+          onPaginationModelChange={handlePaginationModelChange}
+          rowCount={totalDocuments}
+          pageSizeOptions={paginationOption}
+          checkboxSelection
+          disableColumnMenu
+          disableRowSelectionOnClick
+          onRowSelectionModelChange={handleRowSelectionModelChange}
+          rowSelectionModel={selectionModel}
+          onRowClick={handleRowClick}
+          localeText={{
+            footerRowSelected: (count) => `${count} выбрано`,
+            noResultsOverlayLabel: 'Нет результатов',
+            toolbarColumns: 'Столбцы',
+            toolbarColumnsLabel: 'Управление столбцами',
+            toolbarFilters: 'Фильтры',
+            toolbarFiltersLabel: 'Фильтры',
+            toolbarExport: 'Экспорт',
+            toolbarExportLabel: 'Экспортировать',
+            toolbarExportCSV: 'Экспорт в CSV',
+            toolbarExportPrint: 'Печать',
+            toolbarExportExcel: 'Экспорт в Excel',
+          }}
+          slots={{
+            pagination: (props) => (
+              <GridPagination
+                {...props}
+                labelDisplayedRows={({ from, to, count }) => {
+                  return ` ${from}-${to} из ${count}`
+                }}
+                labelRowsPerPage="Документов на странице:"
+              />
+            ),
+            toolbar: () => <GridToolbar buttons={buttons} />,
+          }}
+          sx={{
+            '--DataGrid-overlayHeight': '300px',
+            '& .MuiDataGrid-cell': {
+              '&:focus': {
+                outline: 'none',
               },
-              '& .MuiDataGrid-row': {
-                '&:hover': {
-                  boxShadow: theme.shadows[3],
-                },
-                '&:focus-within': {
-                  backgroundColor: alpha(theme.palette.primary.main, 0.1),
-                },
+            },
+            '& .MuiDataGrid-row': {
+              '&:hover': {
+                boxShadow: theme.shadows[3],
               },
-            }}
-          />
-        </Paper>
-        <Outlet />
-      </>
-    )
-  }
-)
+              '&:focus-within': {
+                backgroundColor: alpha(theme.palette.primary.main, 0.1),
+              },
+            },
+          }}
+        />
+      </Paper>
+      <Outlet />
+    </>
+  )
+}
