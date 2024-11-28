13c67161df58a6b5c26a2b6707a673d9790144ea
gineff <gneff80@gmail.com>
2024-11-28 22:08:33 +0300

    feat: add censor list

--- src/components/signatureBlock/constants.ts
+++ src/components/signatureBlock/constants.ts
@@ -4,3 +4,22 @@ export const modesMap: Record<Modes, string> = {
   [Modes.Signature]: 'Запросить подпись',
   [Modes.Vote]: 'Создать голосование',
 }
+
+export const menuPositionConfig = {
+  anchorOrigin: {
+    vertical: 'bottom' as const,
+    horizontal: 'right' as const,
+  },
+  transformOrigin: {
+    vertical: 'bottom' as const,
+    horizontal: 'left' as const,
+  },
+}
+
+const tomorrow = new Date()
+tomorrow.setDate(tomorrow.getDate() + 1)
+
+export const initialVoteFormValues = {
+  deadline: tomorrow.toLocaleDateString('en-CA'),
+  approvalThreshold: 50,
+}
