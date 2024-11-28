28089d910e79e6d822d9dc193263ca97f57c1ef1
gineff <gneff80@gmail.com>
2024-11-28 22:07:09 +0300

    feat: update signatureBlock

--- src/components/signatureBlock/signatureBlock.tsx
+++ src/components/signatureBlock/signatureBlock.tsx
@@ -1,6 +1,5 @@
-import { FC, useEffect, useState } from 'react'
+import { FC } from 'react'
 import { observer } from 'mobx-react-lite'
-import { useNotifications } from '@toolpad/core/useNotifications'
 import documentsSignService from '@/stores/DocumentsSignService/DocumentsSignService'
 import { DocumentWithSignature } from '@/stores/DocumentsSignService/types'
 import { SignatureViewProps } from './types'
@@ -8,31 +7,9 @@ import { SignByReviewer } from './signByReviewer'
 import { SignByAuthor } from './signByAuthor'
 import { Loader } from '@/components/loader/loader'
 
-export const SignatureBlock: FC<SignatureViewProps> = observer(
-  ({ documentId }) => {
-    const [document, setDocument] = useState<DocumentWithSignature | null>(null)
-    const { loading, error, getDocumentById } = documentsSignService
-    const notifications = useNotifications()
-
-    useEffect(() => {
-      const loadDocument = async () => {
-        if (documentId) {
-          const fetchedDocument = await getDocumentById(documentId)
-          setDocument(fetchedDocument)
-        }
-      }
-
-      void loadDocument()
-    }, [documentId, getDocumentById])
-
-    useEffect(() => {
-      if (error) {
-        notifications.show(error.message, {
-          severity: 'error',
-          autoHideDuration: 2000,
-        })
-      }
-    }, [error, notifications])
+export const SignatureBlock: FC<SignatureBlockProps> = observer(
+  ({ document }) => {
+    const { loading } = documentsSignService
 
     if (loading) return <Loader />
 
@@ -40,36 +17,11 @@ export const SignatureBlock: FC<SignatureViewProps> = observer(
 
     return (
       <>
-        {!document.isUserAuthor && <SignByReviewer document={document} />}
-        {document.isUserAuthor && <SignByAuthor document={document} />}
-
-        {/*false && (
-          <Box>
-            <Button onClick={() => void document.sign()}>sign</Button>
-            <Button onClick={() => void document.sendSignRequest(censors)}>
-              send
-            </Button>
-            <Button onClick={() => void document.startVote()}>send</Button>
-            <Button onClick={() => void document.cancelVote(censors)}>
-              send
-            </Button>
-            <Typography>{document.status}</Typography>
-            <Typography>{JSON.stringify(document.censors)}</Typography>
-            <Button
-              onClick={() =>
-                document.addCensor({
-                  email: 'email',
-                  id: 1,
-                  name: 'name',
-                  roles: [],
-                  surname: 'surname',
-                })
-              }
-            >
-              add censor
-            </Button>
-          </Box>
-        )*/}
+        {document.isUserAuthor ? (
+          <SignByAuthor document={document} />
+        ) : (
+          <SignByReviewer document={document} />
+        )}
       </>
     )
   }
