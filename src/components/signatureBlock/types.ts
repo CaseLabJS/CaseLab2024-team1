aafb13e362bf298d1522d309613610cd54d23da5
gineff <gneff80@gmail.com>
2024-11-28 22:09:07 +0300

    feat: add vote form

--- src/components/signatureBlock/types.ts
+++ src/components/signatureBlock/types.ts
@@ -1,5 +1,6 @@
 import { DocumentWithSignature } from '@/stores/DocumentsSignService'
-import { User } from '@/types/sharedTypes'
+import UserStore from '@/stores/UserStore'
+import { SxProps, Theme } from '@mui/system'
 
 export type SignatureBlockProps = {
   document: DocumentWithSignature
@@ -22,8 +23,27 @@ export type SignatureModeSelectorProps = {
   selectMode: (mode: Modes) => void
 }
 
-export type ManageCensorsListProps = {
-  censors: User[]
-  setCensors: (users: User[]) => void
-  mode: Modes
+export type Censor = InstanceType<typeof UserStore>
+
+export type CensorsListMenuProps = {
+  render: (arg0: Censor[]) => JSX.Element
+  anchorEl: HTMLElement | null
+  setAnchorEl: React.Dispatch<React.SetStateAction<HTMLElement | null>>
+}
+
+export type CensorsListProps = {
+  sx?: SxProps<Theme>
+  censors: Censor[]
+  onCensorSelect: (censor: Censor) => void
+  selected: boolean
+}
+
+export type VoteFormValues = {
+  deadline: string
+  approvalThreshold: number
+}
+
+export type VoteFormProps = {
+  startVote: (formValues: VoteFormValues) => void
+  disabled: boolean
 }
---
+++ src/components/signatureBlock/voteForm.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useForm, FormProvider } from 'react-hook-form'
+import { Box, Button } from '@mui/material'
+import { PercentageSlider } from './percentageSlider'
+import { Modes, VoteFormProps, VoteFormValues } from './types'
+import { initialVoteFormValues, modesMap } from './constants'
+import { DeadlineInput } from './deadlineInput'
+
+export const VoteForm: React.FC<VoteFormProps> = ({ startVote, disabled }) => {
+  const onSubmit = (data: VoteFormValues) => {
+    console.log(data)
+    startVote(data)
+  }
+
+  const methods = useForm<VoteFormValues>({
+    defaultValues: initialVoteFormValues,
+  })
+
+  return (
+    <FormProvider {...methods}>
+      <Box
+        component={'form'}
+        onSubmit={(event) => void methods.handleSubmit(onSubmit)(event)}
+      >
+        <DeadlineInput />
+        <PercentageSlider />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          disabled={disabled}
+        >
+          {modesMap[Modes.Vote]}
+        </Button>
+      </Box>
+    </FormProvider>
+  )
+}
