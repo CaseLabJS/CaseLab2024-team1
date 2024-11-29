864e39933ea750b204a78311776de0240674783b
gineff <gneff80@gmail.com>
2024-11-29 12:55:50 +0300

    fix: remove signature block

--- src/mock/documentForm/documentForm.tsx
+++ src/mock/documentForm/documentForm.tsx
@@ -13,19 +13,17 @@ import { FileInput } from './fileInput/fileInput'
 import { TitleInput } from './titleInput/titleInput'
 import { DescriptionInput } from './descriptionInput/descriptionInput'
 import { DocumentFormProps, FormValues } from './types'
-//import { FormItem } from '@/components/createDocumentForm/types'
 import styles from './documentForm.module.css'
 import { DocumentValues } from './documentValues/documentValues'
 import { observer } from 'mobx-react-lite'
 import ModeEditIcon from '@mui/icons-material/ModeEdit'
-import { SignatureBlock } from '@/components/signatureBlock/signatureBlock'
 
 export const DocumentForm: FC<DocumentFormProps> = observer(({ document }) => {
   const {
     documentData: { documentType, id, documentVersions },
   } = document
 
-  const [version, setVersion] = useState<number>(documentVersions.length - 1)
+  const [version] = useState<number>(documentVersions.length - 1)
 
   const form = useForm<FormValues>({
     defaultValues: {
@@ -68,7 +66,6 @@ export const DocumentForm: FC<DocumentFormProps> = observer(({ document }) => {
           <DocumentValues />
           <FileInput />
         </FormProvider>
-        <SignatureBlock document={document} />
       </Box>
     </Paper>
   )
