7103a0cbee2b604e913d3b2a2303721f2895dd2b
alena_gluschenko <qlushenko36@gmail.com>
2024-12-04 16:06:54 +0300

    implemented document search in /app, added a modal window for small screens, added a search store, stories has been deleted

---
+++ src/components/autocomplete/option.tsx
@@ -0,0 +1,66 @@
+import Box from '@mui/material/Box'
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile'
+import Typography from '@mui/material/Typography'
+import { highlightText } from '@/components/search/documentItem/highlightText.tsx'
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn'
+import { useCallback, useMemo } from 'react'
+
+interface CustomOptionProps<T> {
+  option: T
+  inputValue: string
+  displayFields: (keyof T)[]
+}
+
+export const CustomOption = <T,>(props: CustomOptionProps<T>) => {
+  const { option, inputValue, displayFields, ...otherProps } = props
+
+  const label = useMemo(() => {
+    return displayFields.map((field) => option[field]).join('|')
+  }, [displayFields, option])
+  const [title, description] = label.split('|')
+
+  const shortenStrings = useCallback((string: string, maxLength: number) => {
+    return string.length > maxLength
+      ? `${string.slice(0, maxLength)}...`
+      : string
+  }, [])
+
+  const shortenedDescription = shortenStrings(description, 25)
+  const shortenedTitle = shortenStrings(title, 20)
+
+  return (
+    <Box
+      component="li"
+      {...otherProps}
+      sx={{
+        display: 'flex',
+        borderRadius: '0.5rem',
+        margin: '0 0.5rem',
+        '&.MuiAutocomplete-option': {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        },
+      }}
+    >
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+        <InsertDriveFileIcon className="main-icon" sx={{ color: 'grey.600' }} />
+        <Box>
+          <Typography className="title-text" variant="body1">
+            {highlightText(shortenedTitle, inputValue)}
+          </Typography>
+          <Typography
+            className="description-text"
+            variant="body2"
+            sx={{ fontSize: '0.8rem', color: 'grey.500' }}
+          >
+            {highlightText(shortenedDescription, inputValue)}
+          </Typography>
+        </Box>
+      </Box>
+      <Box>
+        <KeyboardReturnIcon sx={{ opacity: 0 }} className="main-icon" />
+      </Box>
+    </Box>
+  )
+}
---
+++ src/stores/SearchStore/SearchStore.ts
@@ -0,0 +1,50 @@
+import { Document, DocumentVersion } from '@/types/sharedTypes.ts'
+import { SerializedError } from '@/api/core/serializedError.ts'
+import { makeAutoObservable, runInAction } from 'mobx'
+import { executeWithLoading } from '@/utils/executeWithLoading.ts'
+import { documentControllerApi } from '@/api/documentController'
+import documentsListStore from '@/stores/DocumentsListStore'
+
+class SearchStore {
+  allDocuments: Document[] = []
+  loading: boolean = false
+  error: SerializedError | null = null
+  searchHistory: DocumentVersion[] = []
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  fetchAllDocuments = async () => {
+    const { documentsSize, countTotalDocuments } = documentsListStore
+
+    if (!documentsSize) {
+      await countTotalDocuments()
+    }
+
+    await this.loadDocuments(documentsSize)
+  }
+
+  private loadDocuments = async (size: number) => {
+    const fetchedDocuments = await executeWithLoading(this, async () =>
+      documentControllerApi.getDocuments({ size })
+    )
+
+    if (fetchedDocuments) {
+      runInAction(() => {
+        this.allDocuments = fetchedDocuments
+      })
+    }
+  }
+
+  addToSearchHistory = (documentVersion: DocumentVersion) => {
+    if (!this.searchHistory.includes(documentVersion)) {
+      runInAction(() => {
+        this.searchHistory.push(documentVersion)
+      })
+    }
+  }
+}
+
+const searchStore = new SearchStore()
+export default searchStore
